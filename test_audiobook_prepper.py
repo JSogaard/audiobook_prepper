e2c8ee0a33077d7590888218e53f3e91bab390aa
Jonatan Søgaard <jonatanjuhl@icloud.com>
2024-07-13 18:58:47 +0200

    [WIP] Test for number_files

--- test_audiobook_prepper.py
+++ test_audiobook_prepper.py
@@ -1,9 +1,8 @@
 from click.testing import CliRunner
 from mutagen.easyid3 import EasyID3
-from mutagen.id3 import ID3NoHeaderError
 import audiobook_prepper
 
-files: list[str] = [
+FILES: list[str] = [
     "./test_audiobook/01.mp3",
     "./test_audiobook/02.mp3",
     "./test_audiobook/03.mp3",
@@ -11,12 +10,13 @@ files: list[str] = [
     "./test_audiobook/05.mp3",
 ]
 
-def test_number_files_arglist():
+
+
+def test_number_files():
     runner = CliRunner()
     result = runner.invoke(
         audiobook_prepper.number_files,
         [
-            "number",
             "test_audiobook/01.mp3",
             "test_audiobook/02.mp3",
             "test_audiobook/03.mp3",
@@ -31,6 +31,6 @@ def test_number_files_arglist():
     id3: EasyID3
     file: str
     number: int
-    for number, file in enumerate(files, start=2):
+    for number, file in enumerate(FILES, start=2):
         id3 = EasyID3(file)
-        assert id3["tracknumber"] == str(number)
+        assert id3["tracknumber"] == [str(number)]
